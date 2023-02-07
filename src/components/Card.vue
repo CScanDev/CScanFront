4444e368223303945a055b378fbe3af005cd44f3
flightoffancy-b <petuhov.ilja2015@yandex.ru>
2023-02-07 14:37:41 +0400

    Переписал

---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { IUser } from "@/models/IUser"
+
+defineProps<{
+  user: IUser
+}>();
+</script>
+
+<template>
+<div class="card">
+  <div class="card-block">
+    <h3 class="name"><!--{{ product_data.name }}-->{{ user.name }}</h3>
+    <p class="price">Username: <!--{{ product_data.price }}-->{{ user.username }}</p>
+    <p class="description"><!--{{ product_data.description }}--> {{ user.phone }}</p>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+    .card-block{
+        width: 250px;
+        height: 200px;
+        background-color: #fff;
+        border-radius: 5px;
+        padding: 10px;
+        box-shadow: 0 0 8px 0 #000;
+        margin: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .name {
+            background-color: #fff;
+        }
+        .price {
+            background-color: #fff;
+        }
+        
+        .description {
+            background-color: #fff;
+            display: flex;
+            text-align: center;
+            color: gray;
+        }
+    }
+</style>
---
+++ src/components/Catalog.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Card from './Card.vue';
+import type { IUser } from "@/models/IUser"
+
+const props = defineProps<{
+    users: IUser[]
+}>()
+</script>
+
+<template>
+  <div class="catalog">
+    <div class="catalog-container">
+      <Card 
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+      />   
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+    .catalog-container {
+        width: 900px;
+        margin: 40px auto;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+    }
+</style>
+
+
+
