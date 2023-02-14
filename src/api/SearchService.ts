7d6647c1f0d750886e3af08bc6a71072d4f97aaf
flightoffancy-b <petuhov.ilja2015@yandex.ru>
2023-02-14 11:22:01 +0400

    refactoring

--- src/api/SearchService.ts
+++ src/api/SearchService.ts
@@ -3,7 +3,7 @@ import{ HTTP } from "./index"
 export default({
   async getSearchData(value: string){
       try {
-          const response = await HTTP.get('/api/') // params string
+          const response = await HTTP.get('/api') // params string
           return response.data
       } catch (e) {
           console.log(e)
